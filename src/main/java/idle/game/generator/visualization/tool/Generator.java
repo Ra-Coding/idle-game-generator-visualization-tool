1d2c0fd2168be439a92b79e6adf1f9f24bd5a6aa
Fireball19 <fireball1199@googlemail.com>
2021-05-13 00:00:53 +0200

    Add generator class

---
+++ src/main/java/idle/game/generator/visualization/tool/Generator.java
@@ -0,0 +1,19 @@
+package idle.game.generator.visualization.tool;
+
+public class Generator {
+	
+	private double multiplier;
+	private int owned;
+    private double baseCost;
+    private double baseRevenue;
+    private float baseProductionTimeInSeconds;
+    private double costFactor;
+    private Multiplier[] multipliers;
+    private float productionCycleInSeconds;
+    private float productionTimeInSeconds;
+    private double nextBuildingCostsForOne;
+	
+	public Generator() {
+	}
+
+}
