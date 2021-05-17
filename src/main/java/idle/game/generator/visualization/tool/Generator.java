efe8512c2174335b6483fa1b10c6683d004f713f
Fireball19 <fireball1199@googlemail.com>
2021-05-17 17:19:12 +0200

    Add multiplier to produce method

--- src/main/java/idle/game/generator/visualization/tool/Generator.java
+++ src/main/java/idle/game/generator/visualization/tool/Generator.java
@@ -8,12 +8,39 @@ public class Generator {
     private double baseRevenue;
     private float baseProductionTimeInSeconds;
     private double costFactor;
-    private Multiplier[] multipliers;
-    private float productionCycleInSeconds;
-    private float productionTimeInSeconds;
     private double nextBuildingCostsForOne;
 	
-	public Generator() {
+	public Generator(double baseCost, double costFactor, double baseRevenue, float baseProductionTimeInSeconds, double multiplier) {
+		this.baseCost = baseCost;
+		this.costFactor = costFactor;
+		this.baseRevenue = baseRevenue;
+		this.baseProductionTimeInSeconds = baseProductionTimeInSeconds;
+		this.multiplier = multiplier;
+		updateNextBuildingCosts();
 	}
+	
 
+    public void updateNextBuildingCosts() {
+        var kOverR = Math.pow(costFactor, owned);
+        var kPlusNOverR = Math.pow(costFactor, owned + 1);
+
+        nextBuildingCostsForOne = baseCost * ((kOverR - kPlusNOverR) / (1 - costFactor));
+    }
+    
+    public void calculateCosts() {
+    	updateNextBuildingCosts();
+    	owned++;
+    }
+    
+    public double produce() {
+        if (owned == 0) {
+            return 0;
+        }
+
+        return (baseRevenue * owned * multiplier) / baseProductionTimeInSeconds;
+    }
+
+	public double getNextBuildingCostsForOne() {
+		return nextBuildingCostsForOne;
+	}
 }
