ef1e66ecf57ce52c62c93245e83bb3144e2d27b4
Fireball19 <fireball1199@googlemail.com>
2023-03-23 18:31:53 +0100

    Add jackson to dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Main {
   requires javafx.controls;
   requires java.desktop;
+  requires com.fasterxml.jackson.databind;
   opens idle.game.generator.visualization.tool to javafx.graphics;
   exports idle.game.generator.visualization.tool;
 }
